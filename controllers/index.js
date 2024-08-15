83043eee3c546bcb35ff21e0fe417b452e171932
AlejandroM2023 <alejandromeza437@gmail.com>
2024-08-15 10:15:58 -0500

    initial changes

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const apiRoutes = require('./api'); // Import the API routes
 
-router.use('/', apiRoutes); // Use the API routes for the root path
+router.use('/api', apiRoutes); // Use the API routes for the root path
+router.use('/', apiRoutes);
 
 module.exports = router;
