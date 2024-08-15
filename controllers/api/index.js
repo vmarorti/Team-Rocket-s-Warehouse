5a30285169a78d103c3366c8d2ac7759d39db02d
AlejandroM2023 <alejandromeza437@gmail.com>
2024-08-15 13:22:28 -0500

    home route fix

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,7 +3,6 @@ const userRoutes = require('./userAccount-routes');
 const postRoutes = require('./post-routes');
 
 
-router.use('/', homeRoutes); // root
 router.use('/user', userRoutes); // root
 
 module.exports = router;
