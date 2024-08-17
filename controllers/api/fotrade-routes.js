627a6672589d43e072762f37fb0986bd80281401
AlejandroM2023 <alejandromeza437@gmail.com>
2024-08-17 17:26:01 -0500

    adding page for trading

--- controllers/api/fotrade-routes.js
+++ controllers/api/fotrade-routes.js
@@ -1,4 +1,4 @@
-
+const router = require('express').Router();
 
 //set trade to true or false for when user accepts or denies offer (will be null initally)
 router.put('', async (req,res) => {
@@ -11,7 +11,14 @@ router.put('', async (req,res) => {
 });
 
 //set pokemon to offer for a trade which also creates a row in buyer and updates buyer in fortrade as well
-router.put('', async (req,res) => {
+router.put('/offer', async (req,res) => {
+
+        try{
+                console.log(req.body);
+                res.status(200).json(req.body);
+        }catch(err){
+                res.status(500).json(err);
+        }
         /*join for trade,trad and posts where posts.id = req.body.id
                                         or
         make sure card partial had data-fortradeid and use that value to find row
@@ -34,3 +41,5 @@ router.put('', async (req,res) => {
         //set trade to null
         
 });
+
+module.exports = router;
