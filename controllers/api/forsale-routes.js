0a58f95e30e4a95480398a928f3a3dfabeddee9e
AlejandroM2023 <alejandromeza437@gmail.com>
2024-08-17 15:31:48 -0500

    posting card

--- controllers/api/forsale-routes.js
+++ controllers/api/forsale-routes.js
@@ -1,7 +1,8 @@
 
 //create buyer in buyer table and set buyer in forsale
 router.put('', async (req,res) => {
-    //check if for sale or trade
+    //check if byer exits in collector table if not create
+    
         /*join for trade,trad and posts where posts.id = req.body.id
                                         or
         make sure card partial had data-fortradeid and use that value to find row
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,6 +3,8 @@ const userRoutes = require('./userAccount-routes');
 const postRoutes = require('./post-routes');
 
 
-router.use('/user', userRoutes); // root
+router.use('/user', userRoutes);
+router.use('/post', postRoutes);
+
 
 module.exports = router;
