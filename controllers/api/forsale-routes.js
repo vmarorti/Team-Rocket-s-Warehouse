1ced445cd1c8d46588d30c6c642e3fe17bf03a58
AlejandroM2023 <alejandromeza437@gmail.com>
2024-08-18 16:46:20 -0500

    buy button fuctionality and routes

--- controllers/api/forsale-routes.js
+++ controllers/api/forsale-routes.js
@@ -1,12 +1,21 @@
+const router = require('express').Router();
+const {ForSale, Collector} = require('../../models');
 
 //create buyer in buyer table and set buyer in forsale
-router.put('', async (req,res) => {
+router.put('/buy', async (req,res) => {
     //check if byer exits in collector table if not create
-    
-        /*join for trade,trad and posts where posts.id = req.body.id
-                                        or
-        make sure card partial had data-fortradeid and use that value to find row
-        */
-        //update row to true or false
+    try{
+        let collector = await Collector.findOne({where:{buyer_id: req.session.user_id}});
+        if(!collector){
+            collector = await Collector.create({buyer_id: req.session.user_id})
+        }
+        const addBuyer = await ForSale.update({buyer: collector.dataValues.id},
+            {where: {id: req.body.saleid}});                                     
+        res.status(200).json(addBuyer);
+    }catch(err){
+        res.status(500).json(err);
+    }
         
 });
+
+module.exports = router;
